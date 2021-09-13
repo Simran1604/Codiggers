75cdd80cbf6e8749fd1e66694dcaed0746f9abd6
Ipsita <ipshu.satapathy@gmail.com>
2021-09-13 17:32:16 +0530

    day-2 task

---
+++ details.js
@@ -0,0 +1,136 @@
+
+var showBtn1 = document.getElementById("btn1");
+var showBtn2 = document.getElementById("btn2");
+var showBtn3 = document.getElementById("btn3");
+var showBtn4 = document.getElementById("btn4");
+var showBtn5 = document.getElementById("btn5");
+var showBtn6 = document.getElementById("btn6");
+var showBtn7 = document.getElementById("btn7");
+var showBtn8 = document.getElementById("btn8");
+var showBtn9 = document.getElementById("btn9");
+var showBtn10 = document.getElementById("btn10");
+
+
+function myFunction1() {
+    var x = document.getElementById("det1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      showBtn1.innerHTML = "Hide Details"
+    } else 
+    {
+      x.style.display = "none";
+      showBtn1.innerHTML = "See Details"
+    }
+  }
+  function myFunction2() {
+    var x = document.getElementById("det2");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      showBtn2.innerHTML = "Hide Details"
+    } else 
+    {
+      x.style.display = "none";
+      showBtn2.innerHTML = "See Details"
+    }
+  }
+
+  function myFunction3() {
+    var x = document.getElementById("det3");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      showBtn3.innerHTML = "Hide Details"
+    } else 
+    {
+      x.style.display = "none";
+      showBtn3.innerHTML = "See Details"
+    }
+  }
+  function myFunction4() {
+    var x = document.getElementById("det4");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      showBtn4.innerHTML = "Hide Details"
+    } else 
+    {
+      x.style.display = "none";
+      showBtn4.innerHTML = "See Details"
+    }
+  }
+  function myFunction5() {
+    var x = document.getElementById("det5");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      showBtn5.innerHTML = "Hide Details"
+    } else 
+    {
+      x.style.display = "none";
+      showBtn5.innerHTML = "See Details"
+    }
+  }
+  function myFunction6() {
+    var x = document.getElementById("det6");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      showBtn6.innerHTML = "Hide Details"
+    } else 
+    {
+      x.style.display = "none";
+      showBtn6.innerHTML = "See Details"
+    }
+  }
+  function myFunction7() {
+    var x = document.getElementById("det7");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      showBtn7.innerHTML = "Hide Details"
+    } else 
+    {
+      x.style.display = "none";
+      showBtn7.innerHTML = "See Details"
+    }
+  }
+  function myFunction8() {
+    var x = document.getElementById("det8");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      showBtn8.innerHTML = "Hide Details"
+    } else 
+    {
+      x.style.display = "none";
+      showBtn8.innerHTML = "See Details"
+    }
+  }
+  function myFunction9() {
+    var x = document.getElementById("det9");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      showBtn9.innerHTML = "Hide Details"
+    } else 
+    {
+      x.style.display = "none";
+      showBtn9.innerHTML = "See Details"
+    }
+  }
+  function myFunction10() {
+    var x = document.getElementById("det10");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      showBtn10.innerHTML = "Hide Details"
+    } else 
+    {
+      x.style.display = "none";
+      showBtn10.innerHTML = "See Details"
+    }
+  }
+
+  showBtn1.addEventListener("click", myFunction1);
+  showBtn2.addEventListener("click", myFunction2);
+  showBtn3.addEventListener("click", myFunction3);
+  showBtn4.addEventListener("click", myFunction4);
+  showBtn5.addEventListener("click", myFunction5);
+  showBtn6.addEventListener("click", myFunction6);
+  showBtn7.addEventListener("click", myFunction7);
+  showBtn8.addEventListener("click", myFunction8);
+  showBtn9.addEventListener("click", myFunction9);
+  showBtn10.addEventListener("click", myFunction10);
+  
